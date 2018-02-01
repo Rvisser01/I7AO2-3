2ad93cf51e83010f8470327ecd1761e0e41b5c12
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-02-01 10:12:36 +0100

    ij9

--- GroepsWebsite/Groep/damian.php
+++ GroepsWebsite/Groep/damian.php
@@ -9,7 +9,7 @@
   <body>
     <div class="mx-auto" style="width: 288px;">
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="../img/damian.jpg" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">Profile</h5>
           <p class="card-text">Naam: Damian <br>Klas: I7AO2<br>Leeftijd: 16<br></p>
