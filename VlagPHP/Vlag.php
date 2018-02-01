d6c05e90420a8c4eae044d00bf64c5b5f612b0ef
Rvisser01 <rvisser01@student.rocvantwente.nl>
2018-02-01 14:01:02 +0100

    VlagPHP

---
+++ VlagPHP/Vlag.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Vlag</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <center>
+    <?php
+    echo '
+    <hr class="rood">
+    <hr class="wit">
+    <hr class="blauw">';
+
+    ?>
+  </center>
+  </body>
+</html>
