92895a032393f56ec087b70446cdad8931389425
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-02-05 12:27:51 +0100

    ;

--- GroepsWebsite/Groep/damian.php
+++ GroepsWebsite/Groep/damian.php
@@ -1,3 +1,4 @@
+<<<<<<< current
 <!DOCTYPE html>
 <html>
   <head>
@@ -49,3 +50,56 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
+=======
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="stylegroep3.css">
+    <meta charset="utf-8">
+    <title>Damian Trojak</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+  </head>
+  <body>
+    <div class="card text-center">
+<div class="card-header">
+  <ul class="nav nav-pills card-header-pills">
+    <li class="nav-item">
+      <a class="nav-link" href="HoofdPagina.html">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Laurence.php">Laurence</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Arben.php">Arben</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Robbin.php">Robbin</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" href="Damian.php">Damian</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Pim.php">Pim</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Peter.php">Peter</a>
+    </li>
+  </ul>
+</div>
+    <div class="mx-auto" style="width: 288px;">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../img/damian.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Profile</h5>
+          <p class="card-text">Naam: Damian <br>Klas: I7AO2<br>Leeftijd: 16<br></p>
+        </div>
+      </div>
+    </div>
+    <!-- Bootstrap javascript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+>>>>>>> before discard
