63a03947a424e299cf3caf21f27a816758270563
Rvisser01 <rvisser01@student.rocvantwente.nl>
2018-02-05 14:28:06 +0100

    Thisisme

---
+++ Thisisme/thisisme.php
@@ -0,0 +1,15 @@
+<?php
+
+  $voornaam = "Robbin";
+  $achternaam = "Visser";
+  $adres = "Kerkhofweg";
+  $huisnummer = "43";
+  $plaats = "Overdinkel";
+  $leeftijd = "16";
+
+echo "
+    <img src='img\ava.png' width='100' height='100'></img>
+    <h1>Naam: $voornaam $achternaam</h1>
+    <h2>adres: $adres $huisnummer $plaats</h2>
+    <h3>leeftijd: $leeftijd</h3>"
+?>
