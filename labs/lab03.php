7ca41f2440fc5bb48eaf24dc0fd4683ff4fc8bf2
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-02-06 10:21:40 +0100

    b

--- labs/lab03.php
+++ labs/lab03.php
@@ -9,11 +9,14 @@
 $playslist =array (
 array("genre" => "Hiphop","Auteur" => "John Williams", "Titel" => "My Girl"),
 array("genre" => "Jazz","Auteur" => "John Coltrane", "Titel" => "New York"),
-array("genre" => "Hiphop","Auteur" => "Shakira", "Titel" => "Obsession"),
-array("genre" => "Latin","Auteur" => "Caetano Veloso", "Titel" => "Cafe Atlantico")
+array("genre" => "Hiphop","Auteur" => "Shakira", "Titel" => "Obsession")
 );
+echo
+printTracks ($item, $key);{
 
+}
 
+array_walk($playlist, "printTracks");
 
  ?>
   </body>
