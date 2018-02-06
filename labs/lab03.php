dc7403a84d87ac55fda72a65afc9c8c24e4a6baf
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-02-06 12:31:14 +0100

    fdhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- labs/lab03.php
+++ labs/lab03.php
@@ -6,22 +6,15 @@
    </head>
    <body>
 <?php
+function myfunction($value,$key)
+{echo
 
 $playslist =array (
 array("genre" => "Hiphop","Auteur" => "John Williams", "Titel" => "My Girl"),
 array("genre" => "Jazz","Auteur" => "John Coltrane", "Titel" => "New York"),
-array("genre" => "Hiphop","Auteur" => "Shakira", "Titel" => "Obsession"),
-array("genre" => "Latin","Auteur" => "Caetano Veloso", "Titel" => "Cafe Atlantico")
+array("genre" => "Hiphop","Auteur" => "Shakira", "Titel" => "Obsession")
+array_walk_recursive($a2,"myfunction");
 );
-{
-echo "string";
-}
-printTracks ($item, $key);{
-
-}
-
-
 
  ?>
-  </body>
 </html>
