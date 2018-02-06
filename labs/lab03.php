7176e53e146ddd2df85fc50f9e0f23065462bc72
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-02-06 09:32:31 +0100

    .

---
+++ labs/lab03.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>php lab 03</title>
+   </head>
+   <body>
+<?php
+$playslist =array (
+array("genre" => "hiphop","auteur" => "John Williams", "titel" => "My Girl")
+array("genre" => "Jazz","auteur" => "John Coltrane", "titel" => "New York")
+array("genre" => "hiphop","auteur" => "Shakira", "titel" => "Obsession")
+array("genre" => "Latin","auteur" => "Caetano Veloso", "titel" => "Cafe Atlantico")
+);
+
+
+
+ ?>
+  </body>
+</html>
