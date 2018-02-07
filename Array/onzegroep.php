5d8632544a967dc411b3e7f2a06ad0274959066d
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-02-07 11:31:40 +0100

    asd

--- Array/onzegroep.php
+++ Array/onzegroep.php
@@ -6,7 +6,12 @@
   </head>
   <body>
     <?php
-      $voornaam = array('0266931' => Peter , '0267730' => Pim, '0279410' => Arben, '0297652' => Robbin, '0' => Peter, '0266931' => Peter);
+    $voornaam = array( '0300013' => Laurence , '0266931' => Peter , '0267730' => Pim, '0279410' => Arben, '0297652' => Robbin, '0300838' => Damian);
+    $achternaam = array( '0300013' => Van der Wel , '0266931' => Kuipers , '0267730' => Hoomans, '0279410' => Hajrizaj, '0297652' => Visser, '0300838' => Trojak);
+    $woonplaats = array( '0300013' => Enschede , '0266931' => Enschede , '0267730' => Enschede, '0279410' => Enschede, '0297652' => Overdinkel, '0300838' => Enschede);
+    $Leeftijd = array( '0300013' => 16 , '0266931' => 20 , '0267730' => 20, '0279410' => 19, '0297652' => 16, '0300838' => 16);
+
+    print_r(array_rand(,))
      ?>
   </body>
 </html>
