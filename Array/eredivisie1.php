69bd522baec0f0115e467e82411f75212f98cbed
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-02-07 11:35:04 +0100

    .

--- Array/eredivisie1.php
+++ Array/eredivisie1.php
@@ -5,8 +5,9 @@
     <title>Eredivisie</title>
   </head>
   <body>
-<?php
-
-?>
+    <h1>De stand van 07-02-2018 om 12:00 uur</h1>
+    <?php
+        $positie = array('PSV Eindhoven' => 1 , 'Ajax' => 2 , 'AZ Alkmaar' => 3 , 'PEC Zwolle' => 4 , 'Feyenoord' => 5 , 'Vitesse' => 6 , 'FC Utrecht' => 7 , 'Heerenveen' => 8 ,  'VVV-Venlo' => 9 , 'ADO Den Haag' => 10 , 'Excelsior' => 11 , 'Heracles Almelo' => 12 , 'FC Groningen' => 13 , 'Willem II' => 14 , 'FC Twente' => 15 , 'NAC Breda' => 16 , 'Roda JC Kerkrade' => 17 , 'Sparta Rotterdam' => 18 , );
+       ?>
   </body>
 </html>
