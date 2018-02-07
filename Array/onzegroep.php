5987cdb99e0a51362f789ca383e3faba5e0c6cc0
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-02-07 11:23:09 +0100

    sad

--- Array/onzegroep.php
+++ Array/onzegroep.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <?php
-      $voornaam = array('0266931' => Peter , '0267730' => Pim, '0279410' => Arben, '0297652' => Peter, '0266931' => Peter, '0266931' => Peter);
+      $voornaam = array('0266931' => Peter , '0267730' => Pim, '0279410' => Arben, '0297652' => Robbin, '0' => Peter, '0266931' => Peter);
      ?>
   </body>
 </html>
