193cb0d4e784ec57311dff3a7d52a7f5fbc941c9
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-02-07 11:17:45 +0100

    asd

---
+++ Array/onzegroep.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Onze groep</title>
+  </head>
+  <body>
+    <?php
+      $voornaam = array('0266931' => Peter , '0267730' => Pim, '0279410' => Arben, '0297652' => Peter, '0266931' => Peter, '0266931' => Peter);
+     ?>
+  </body>
+</html>
