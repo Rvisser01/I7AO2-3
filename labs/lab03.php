c2c0bf0988c4ad673d621f6bd1ea78a30c66141a
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-02-07 16:44:06 +0100

    sac

--- labs/lab03.php
+++ labs/lab03.php
@@ -13,8 +13,8 @@ $playslist =array (
 array("genre" => "Hiphop","Auteur" => "John Williams", "Titel" => "My Girl"),
 array("genre" => "Jazz","Auteur" => "John Coltrane", "Titel" => "New York"),
 array("genre" => "Hiphop","Auteur" => "Shakira", "Titel" => "Obsession")
-array_walk_recursive($a2,"myfunction");
-);
+array_walk_recursive($playslist,"myfunction");
+}
 
  ?>
 </html>
