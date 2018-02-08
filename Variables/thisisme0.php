1f73bb3115e018fac341422729e7d163f66d5698
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-02-08 10:00:02 +0100

    das

---
+++ Variables/thisisme0.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+<?php
+
+  $voornaam = "Robbin";
+  $achternaam = "Visser";
+  $adres = "Kerkhofweg";
+  $huisnummer = "43";
+  $plaats = "Overdinkel";
+  $leeftijd = "16";
+
+echo "
+    <img src='img\ava.png' width='100' height='100'></img>
+    <h1>Naam: $voornaam $achternaam</h1>
+    <h2>adres: $adres $huisnummer $plaats</h2>
+    <h3>leeftijd: $leeftijd</h3>"
+?>
+</body>
+</html>
