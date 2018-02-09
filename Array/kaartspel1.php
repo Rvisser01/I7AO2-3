cb37de676666d093b7e822043bb1fb26ae847153
AHajrizaj <35994818+AHajrizaj@users.noreply.github.com>
2018-02-09 11:00:09 +0100

    cxz

--- Array/kaartspel1.php
+++ Array/kaartspel1.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+    <title>Kaartspel</title>
+    <link " rel="stylesheet">
+    <style media="screen">
+        * {
+            font-family: 'Lato', sans-serif;
+        }
+    </style>
+</head>
+<body>
+<center>
+    <form class="" method="post">
+        <input type="submit" name="button" value="Spin">
+    </form>
+    <br>
     <?php
-    
-     ?>
-  </body>
+
+    if (isset($_POST['button'])) {
+j
+        $kleuren = array('ruiten', 'harten', 'klaveren', 'schoppen');
+        $waarden = array('vrouw', 'heer', 'boer', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1');
+
+        for ($x = 0; $x <= 4; $x++) {
+            $randKleur = array_rand($kleuren);
+            $randWaarde = array_rand($waarden);
+
+            echo "<img src='kaarten/" . $kleuren[$randKleur] ."_" . $waarden[$randWaarde] .".svg'></img>";
+        }
+    }
+
+    ?>
+</center>
+</body>
 </html>
