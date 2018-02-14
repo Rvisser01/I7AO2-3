0a364f8eb82b0add1362f03b6574c3afc5468207
Rvisser01 <rvisser01@student.rocvantwente.nl>
2018-02-14 09:06:18 +0100

    Groepswebsite2.0

---
+++ GroepsWebsite2.0/Groep/peter.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Peter Kuipers</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+  </head>
+  <body>
+    <div class="card text-center">
+<div class="card-header">
+  <ul class="nav nav-pills card-header-pills">
+    <li class="nav-item">
+      <a class="nav-link" href="../HoofdPagina.html">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Laurence.php">Laurence</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Arben.php">Arben</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Robbin.php">Robbin</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Damian.php">Damian</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Pim.php">Pim</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" href="Peter.php">Peter</a>
+    </li>
+  </ul>
+</div>
+    <div class="mx-auto" style="width: 288px;">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../img/peter.jpeg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Profile</h5>
+          <p class="card-text">Naam: Peter <br>Klas: I7AO2<br>Leeftijd: 20</p>
+        </div>
+      </div>
+    </div>
+    <!-- Bootstrap javascript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ GroepsWebsite2.0/Groep/robbin.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Robbin Visser</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+  </head>
+  <body>
+    <div class="card text-center">
+<div class="card-header">
+  <ul class="nav nav-pills card-header-pills">
+    <li class="nav-item">
+      <a class="nav-link" href="../HoofdPagina.html">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Laurence.php">Laurence</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Arben.php">Arben</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" href="Robbin.php">Robbin</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Damian.php">Damian</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Pim.php">Pim</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Peter.php">Peter</a>
+    </li>
+  </ul>
+</div>
+<br>
+    <div class="mx-auto" style="width: 288px;">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../img/robbin.jpeg" alt="Card image cap">
+        <div class="card-body">
+          <h2 class="card-title">Profile</h2>
+          <p class="card-text">Naam: Robbin <br>Klas: I7AO2<br>Leeftijd: 16</p>
+        </div>
+      </div>
+    </div>
+    <br>
+    <center>
+    <h2>Projecten</h2>
+    </center>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <div class="card">
+
+        <div class="card-body">
+          <h3 class="card-title">Sprint 0</h3>
+          <p class="card-text">
+            <div class="card-block">
+              <a href="#" class="btn btn-primary button">VlagPHP</a>
+              <div class="card-block">
+                <br>
+                <a href="#" class="btn btn-primary button">WeekPHP</a>
+              </div>
+              </div>
+              </div>
+
+</div>
+</div>
+<div class="col-sm">
+  <div class="card">
+
+    <div class="card-body">
+      <h3 class="card-title">Sprint 1</h3>
+      <p class="card-text">
+        <div class="card-block">
+          <a href="#" class="btn btn-primary button">Thisisme</a>
+          <div class="card-block">
+            <br>
+            <a href="#" class="btn btn-primary button">Quotes</a>
+            <div class="card-block">
+            <br>
+            <a href="#" class="btn btn-primary button">Scope</a>
+            <div class="card-block">
+            <br>
+            <a href="#" class="btn btn-primary button">Compare</a>
+            <div class="card-block">
+            <br>
+            <a href="#" class="btn btn-primary button">Operators</a>
+            <div class="card-block">
+            <br>
+            <a href="#" class="btn btn-primary button">Moreoperators</a>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+    </div>
+</div>
+<div class="col-sm">
+  <div class="card">
+
+    <div class="card-body">
+      <h3 class="card-title">Sprint 2</h3>
+      <p class="card-text">
+        <div class="card-block">
+          <a href="#" class="btn btn-primary button">VlagPHP</a>
+          <div class="card-block">
+            <br>
+            <a href="#" class="btn btn-primary button">WeekPHP</a>
+            <div class="card-block">
+              <br>
+              <a href="#" class="btn btn-primary button">WeekPHP</a>
+            </div>
+          </div>
+          </div>
+          </div>
+    </div>
+</div>
+</div>
+</div>
+      </div>
+    </div>
+    <!-- Bootstrap javascript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
