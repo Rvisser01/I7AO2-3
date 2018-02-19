6efaf00fbe6a249af85a5b86bfd3f7f40f0d437b
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-02-19 11:46:37 +0100

    sad

---
+++ Laurence/Controle-structuur/oneven.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>oneven Laurence</title>
+  </head>
+  <body>
+    <?php
+      $oneven = "";
+
+      for ($i = 1; $i <= 100; $i++)
+      {
+        if($i % 2 == 0){ continue; }
+        $oneven .= $i. "<br>";
+      }
+
+echo $var;
+
+     ?>
+  </body>
+</html>
