9195c2e4d68a5b92b5c8700895746b4828e0b897
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-02-20 12:17:52 +0100

    ''

---
+++ Damian/onzegroep2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Onze groep</title>
+  </head>
+  <body>
+    <?php
+      $voornaam = array( '0300013' => "Laurence" , '0266931' => "Peter" , '0267730' => "Pim" , '0279410' => "Arben" , '0297652' => "Robbin" , '0300838' => "Damian");
+      $achternaam = array( '0300013' => "Van der Wel" , '0266931' => "Kuipers" , '0267730' => "Hoomans" , '0279410' => "Hajrizaj" , '0297652' => "Visser" , '0300838' => "Trojak");
+      $woonplaats = array( '0300013' => "Enschede" , '0266931' => "Enschede" , '0267730' => "Enschede" , '0279410' => "Enschede" , '0297652' => "Overdinkel" , '0300838' => "Enschede");
+      $leeftijd = array( '0300013' => "16" , '0266931' => "20" , '0267730' => "20" , '0279410' => "19" , '0297652' => "16" , '0300838' => "16");
+
+      $random = array_rand($voornaam, 1);
+        echo "Studenummer: $random"."<br>";
+        echo "Voornaam: $voornaam[$random]"."<br>";
+        echo "Achternaam $achternaam[$random]"."<br>";
+        echo "Woonplaats $woonplaats[$random]"."<br>";
+        echo "Leeftijd: $leeftijd[$random]"."<br> <br>";
+
+        foreach ($voornaam as $value) {
+          echo "$value <br>";
+}
+
+    ?>
+  </body>
+</html>
