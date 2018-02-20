25c7be3c531d87622b5ac9b63079eb249a0c8856
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-02-20 09:09:04 +0100

    asd

---
+++ Laurence/Controle-structuur/fibonacci.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Fibonacci</title>
+  </head>
+  <body>
+    <?php
+    echo "0; 1; 1; 2; 3; 5; 8; 13; 21; 34; 55; 89; 144; 233; 377; 610; 987; 1597; 2584; 4181";
+    ?>
+  </body>
+</html>
