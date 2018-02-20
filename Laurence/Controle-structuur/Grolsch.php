f1bdfc9c2da8ebb2c2c5587a9568baad8464b83b
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-02-20 10:59:45 +0100

    sda

--- Laurence/Controle-structuur/Grolsch.php
+++ Laurence/Controle-structuur/Grolsch.php
@@ -1,25 +1,14 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Grolsch</title>
-  </head>
-  <body>
     <?php
-      $leeftijd = array("10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30");
-      $randleeftijd = array_rand($leeftijd,1);
-      if ($randleeftijd > 17) {
-        header("http://www.grolsch.nl");
 
-      } elseif ($randleeftijd < 18) {
-      header("https://www.alcoholinfo.nl/publiek");
-      exit();
+      $leeftijd = rand(10,30);
+
+      if ($leeftijd > 18)
+      {
+        header("Location: http://www.grolsch.nl");
+
+      } else {
+
+            header("Location: https://www.alcoholinfo.nl/publiek");
     }
 
-
   ?>
-
-
-    ?>
-  </body>
-</html>
