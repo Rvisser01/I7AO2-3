e877b629b2b3f806480909597fb447bb47aa58b1
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-02-22 13:32:20 +0100

    fds

--- Laurence/Controle-structuur/yahtzee.php
+++ Laurence/Controle-structuur/yahtzee.php
@@ -1,34 +1,44 @@
+<?php
+  $Dobbelsteen = array(rand(1,6));
+
+  function Dobbelsteen1($Dobbelsteen)
+  {
+    $RandomDobbelsteen = array_rand($Dobbelsteen,1);
+    echo "$RandomDobbelsteen";
+  }
+
+  function Dobbelsteen2($dobbelsteen)
+  {
+    $RandomDobbelsteen = array_rand($Dobbelsteen,1);
+    echo "$RandomDobbelsteen";
+  }
+
+  function Dobbelsteen3($dobbelsteen)
+  {
+    $RandomDobbelsteen = array_rand($Dobbelsteen,1);
+    echo "$RandomDobbelsteen";
+  }
+
+  function Dobbelsteen4($dobbelsteen)
+  {
+    $RandomDobbelsteen = array_rand($Dobbelsteen,1);
+    echo "$RandomDobbelsteen";
+  }
+
+  function Dobbelsteen5($dobbelsteen)
+  {
+    $RandomDobbelsteen = array_rand($Dobbelsteen,1);
+    echo "$RandomDobbelsteen";
+  }
+ ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Dobbel Opdracht 1</title>
+		<title>Dobbel Opdracht 2</title>
 	</head>
 	<body>
-		<form action="" method="post">
-			<input type="submit" name="dobbel" value="Dobbel">
-		</form>
-		<?php
-    $array = array();
-			if(isset($_POST['dobbel'])){
-
-
-				for($x = 0; $x < 5; $x++){
-					array_push($array,rand(1,6));
-				}
-			}
-		?>
-		<hr>
-		<?php
-			$totaal = 0;
-
-			echo '<p>Dobbels Gegooid: ';
-
-			foreach($array as $worp){
-				echo '<b>'.$worp.'</b> ';
-				$totaal = $totaal + $worp;
-			}
-
-			echo 'totaal: <b>'.$totaal.'</b></p>';
-		?>
+    <?php
+      Dobbelsteen1();
+    ?>
 	</body>
 </html>
