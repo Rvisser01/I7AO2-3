1c80de7eb084bf9b1f1dbe0cd0ff6f09732108d0
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-02-23 14:28:32 +0100

    dsf

--- labs/lab_06.php
+++ labs/lab_06.php
@@ -1,6 +1,8 @@
 <?php
-  $brief = array("<<abbonnee>>");
-  echo "Beste <b><<abonnee>></b><br>
+  $search = array("<<abbonnee>>", "<<korting>>");
+  $replace = array("Jan Davids", "50");
+
+  $subject = "Beste <b><<abbonnee>></b><br>
     U heeft het laatste nummer van ons magazine ontvangen.<br>
     Omdat we u heel graag als abonnee willen behouden, bieden we u een aantrekke-<br>
     lijke en exclusieve korting: <br>U betaalt <b><<korting>></b> in plaats van 65
@@ -10,5 +12,6 @@
     Sam Simons<br>
     Hoofdredacteur<br>";
 
-$woorden = str_replace("<<abbonnee>>", "Jan Davids", $brief);
-?>
+$boi = str_replace($search, $replace, $subject);
+echo "$boi";
+ ?>
