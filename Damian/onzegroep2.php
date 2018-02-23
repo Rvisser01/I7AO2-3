47c6553acc2ce36451431190cc0febc73c55fe3b
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-02-23 11:35:50 +0100

    f

--- Damian/onzegroep2.php
+++ Damian/onzegroep2.php
@@ -6,20 +6,47 @@
   </head>
   <body>
     <?php
-      $voornaam = array( '0300013' => "Laurence" , '0266931' => "Peter" , '0267730' => "Pim" , '0279410' => "Arben" , '0297652' => "Robbin" , '0300838' => "Damian");
-      $achternaam = array( '0300013' => "Van der Wel" , '0266931' => "Kuipers" , '0267730' => "Hoomans" , '0279410' => "Hajrizaj" , '0297652' => "Visser" , '0300838' => "Trojak");
-      $woonplaats = array( '0300013' => "Enschede" , '0266931' => "Enschede" , '0267730' => "Enschede" , '0279410' => "Enschede" , '0297652' => "Overdinkel" , '0300838' => "Enschede");
-      $leeftijd = array( '0300013' => "16" , '0266931' => "20" , '0267730' => "20" , '0279410' => "19" , '0297652' => "16" , '0300838' => "16");
+      $Voornamen = array( '0300013' => "Laurence" , '0266931' => "Peter" , '0267730' => "Pim" , '0279410' => "Arben" , '0297652' => "Robbin" , '0300838' => "Damian");
+      $Achternamen = array( '0300013' => "Van der Wel" , '0266931' => "Kuipers" , '0267730' => "Hoomans" , '0279410' => "Hajrizaj" , '0297652' => "Visser" , '0300838' => "Trojak");
+      $Woonplaatsen = array( '0300013' => "Enschede" , '0266931' => "Enschede" , '0267730' => "Enschede" , '0279410' => "Enschede" , '0297652' => "Overdinkel" , '0300838' => "Enschede");
+      $Leeftijden = array( '0300013' => "17" , '0266931' => "20" , '0267730' => "20" , '0279410' => "19" , '0297652' => "16" , '0300838' => "16");
 
-      $random = array_rand($voornaam, 1);
-        echo "Studenummer: $random"."<br>";
-        echo "Voornaam: $voornaam[$random]"."<br>";
-        echo "Achternaam $achternaam[$random]"."<br>";
-        echo "Woonplaats $woonplaats[$random]"."<br>";
-        echo "Leeftijd: $leeftijd[$random]"."<br> <br>";
 
-        foreach ($voornaam as $value) {
-          echo "$value <br>";
+      foreach ($Voornamen as $Voornaam){
+          echo "<table>
+            <tr>
+              <td>
+                $Voornaam
+                </td>
+            </tr>
+          </table>";
+      }
+      foreach($Achternamen as $Achternaam){
+        echo "<table>
+          <tr>
+            <td>
+              $Achternaam
+            </td>
+          </tr>
+        </table>";
+      }
+      foreach($Woonplaatsen as $Woonplaats){
+        echo "<table>
+          <tr>
+            <td>
+              $Woonplaats
+            </td>
+          </tr>
+        </table>";
+      }
+      foreach($Leeftijden as $Leeftijd){
+        echo "<table>
+          <tr>
+            <td>
+              $Leeftijd
+            </td>
+          </tr>
+        </table>";
 }
 
     ?>
