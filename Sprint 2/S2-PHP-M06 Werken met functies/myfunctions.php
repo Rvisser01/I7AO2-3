3ee5d8f436710d72b4293c551926f212902bac4a
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-06 10:51:24 +0100

    sda

--- Sprint 2/S2-PHP-M06 Werken met functies/myfunctions.php
+++ Sprint 2/S2-PHP-M06 Werken met functies/myfunctions.php
@@ -1,11 +1,35 @@
 <!DOCTYPE html>
 <html>
-<body>
+  <body>
+    <form action="" method="post">
+     <input type="text" name="getal" placeholder="prime checker";><br>
+     <input type="submit" name="submit" value="Check">
+    <?php
+      function reverse()
+      {
+        $a=array("a"=>"1","b"=>"2","c"=>"3");
+        print_r(array_reverse($a));
+      }
 
-<?php
-$a=array("a"=>"1","b"=>"2","c"=>"3");
-print_r(array_reverse($a));
-?>
+    function IsPrime($n)
+    {
 
-</body>
+     for($x=2; $x<$n; $x++)
+       {
+          if($n %$x ==0)
+    	      {
+    		   return 0;
+    		  }
+        }
+      return 1;
+     }
+    $a = IsPrime(isset($_POST["getal"]));
+    if ($a==0)
+    echo 'false'."\n";
+    else
+    echo 'true'."\n";
+
+    ?>
+
+  </body>
 </html>
