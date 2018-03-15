7de9f7421b3fb269c305f0829aa53fa29ec8d5d0
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-15 14:12:38 +0100

    sda

--- Sprint_3/S2-BE-08 - Werken met datums/lab15.php
+++ Sprint_3/S2-BE-08 - Werken met datums/lab15.php
@@ -1,9 +1,11 @@
 <?php
 // date_diff
-$factuurdatum = date_create("2018-10-10");
-echo "<br>Factuurdatum: ".date_format($factuurdatum, "Y-m-d");
 $vandaag = date_create("now");
-echo "<br>Vandaag: ".date_format($vandaag, "Y-m-d");
-$interval = date_diff($vandaag, $factuurdatum);
-echo "<br>U heeft nog: ".$interval->format("%R%a dagen om te betalen");
+echo "<br>Datum van vandaag: ".date_format($vandaag, "Y-m-d");
+echo "<br>";
+$kerst = date_create("2018-12-25");
+echo "<br>Datum van kerst: ".date_format($kerst, "Y-m-d");
+echo "<br>";
+$interval = date_diff($vandaag, $kerst);
+echo "<br>Er zijn nog ".$interval->format("%a dagen tot kerst");
 ?>
