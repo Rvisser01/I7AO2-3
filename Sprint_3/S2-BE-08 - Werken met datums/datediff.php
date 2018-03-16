046f5a96ba3268ad01f389f34f32c59c2e491afd
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-16 17:50:21 +0100

    bibnibiibi

--- Sprint_3/S2-BE-08 - Werken met datums/datediff.php
+++ Sprint_3/S2-BE-08 - Werken met datums/datediff.php
@@ -7,16 +7,27 @@
   <body>
     <h3>Datediff</h3>
     <form method="post">
-      <input type="text" name="datum1" value="Voer een datum in"><br>
-      Tot<br> <input type="text" name="datum2" value="Voer een datum in">
+      <input type="text" name="datum1" placeholder="Voer een datum in"><br>
+      Tot<br> <input type="text" name="datum2" placeholder="Voer een datum in">
       <br><br><input type="submit" name="bereken" value="Bereken">
     </form>
     <?php
       if (isset($_POST["bereken"])) {
-
-          $datum2 = $_POST['datum2'];
+        if (!(empty($_POST["datum1"]))) {
+          echo "Fill in a date!";
+        }
+        if (!(empty($_POST["datum2"]))) {
+          echo "Fill in a date!";
+        }
           $datum1 = $_POST['datum1'];
-      }  
+          $datum2 = $_POST['datum2'];
+
+          $datum1 = DateTime($datum1);
+          $datum2 = DateTime($datum2);
+          
+          $verschil = date_diff($datum1,$datum2);
+          echo "<br>Er zitten ".$verschil->format("%a dagen verschil tussen");
+      }
 
 
 
--- Sprint_3/S2-BE-08 - Werken met datums/myage.php
+++ Sprint_3/S2-BE-08 - Werken met datums/myage.php
@@ -7,8 +7,19 @@
   </head>
   <body>
     <?php
-      $verjaardag = date_create("2019-03-14");
-      echo(strftime(b,$verjaardag));
+    $verjaardag = date_create("2001-03-14");
+    echo "<br>De dag dat ik ben geboren: ".date_format($verjaardag, "Y-m-d");
+    echo "<br>";
+    $vandaag = date_create("now");
+    echo "<br>De dag van vandaag: ".date_format($vandaag, "Y-m-d");
+    echo "<br>";
+    $interval = date_diff($vandaag, $verjaardag);
+    echo "<br>Ik ben ".$interval->format("%M%m maanden oud");
+    echo "<br>Ik ben ".$interval->format("%a dagen oud");
+    echo "<br>Ik ben ".$interval->format("%m weken oud");
+    echo "<br>Ik ben ".$interval->format("%H%h uren oud");
+    echo "<br>Ik ben ".$interval->format("%F minuten oud");
+    echo "<br>Ik ben ".$interval->format("%f seconden oud");
     ?>
   </body>
 </html>
