eb0d36784b21b941660df499811c8a61706c7cfc
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-16 09:06:32 +0100

    sad

--- Sprint_3/S2-BE-08 - Werken met datums/myage.php
+++ Sprint_3/S2-BE-08 - Werken met datums/myage.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="nl" dir="ltr">
   <head>
+    <link rel="stylesheet" type="text/css" href="myage.css">
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
     <?php
-
-      ?>
+      $verjaardag = date_create("2019-03-14");
+      echo(strftime(b,$verjaardag));
+    ?>
   </body>
 </html>
