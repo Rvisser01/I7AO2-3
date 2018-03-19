d6c511e830aafff575d16361348d9e93d0e859ae
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-19 14:28:43 +0100

    das

--- Sprint_3/S2-BE-09 - Cookies/lab16.php
+++ Sprint_3/S2-BE-09 - Cookies/lab16.php
@@ -1,31 +1,59 @@
 <!DOCTYPE html>
-<html lang="nl">
-  <head>
+<html>
+<head>
+    <meta charset="utf-8">
     <title>Cookies</title>
-  </head>
-  <body>
-    <form name="login" action="" method="POST">
-      Gebruikersnaam:<br>
-      <input type="text" name="user"><br><br>
-      <input type="submit" name="submit" value="inloggen">
-    </form>
-    <?php
-    if (isset($_POST["submit"])) {
-      $Gebruikers = array();
-      ob_start();
-        $Peter = ($_POST["gebruiker"]);
-        setcookie("gebruiker", $Peter, mktime(0, 0, 00, 3, 25, 2018));
-        $Peter = ($_POST["gebruiker"]);
-        $gebruiker = $_COOKIE["gebruiker"];
-      ob_end_flush();
-        if( $gebruiker == $Peter) {
-          echo "<br> $Peter  welkom terug gebruiker";
-        }
-        else {
-          echo "<br> Hallo ", $Peter, " uw bent onze nieuwste gebruiker";
-          array_push($Gebruikers, $Peter);
-        }
-      }
-    ?>
-  </body>
+</head>
+<body>
+<form name="login" method="post">
+    Gebruikersnaam:<br>
+    <input type="text" name="user"/><br><br>
+    <input type="submit" name="submit" value="Inloggen">
+    <input type="submit" name="remove" value="Remove">
+    <input type="submit" name="cookies" value="Show">
+</form>
+<?php
+
+if (isset($_POST['submit'])) {
+    $user = $_POST['user'];
+    welkom($user);
+}
+
+if (isset($_POST['remove'])) {
+    $user = $_POST['user'];
+    RemoveCookie($user);
+}
+
+if (isset($_POST['cookies'])) {
+    showCookies();
+}
+
+function welkom($gebruiker) {
+    if(isset($_COOKIE[$gebruiker])) {
+        $number = $_COOKIE[$gebruiker] + 1;
+        setcookie($gebruiker, $number);
+        echo "<script>alert('Hallo $gebruiker, welkom terug - $number');</script>";
+    } else {
+        setcookie($gebruiker, 1);
+        $number = $_COOKIE[$gebruiker];
+        echo "<script>window.alert('Hallo $gebruiker, u bent onze nieuwste gebruiker - $number');</script>";
+    }
+}
+
+function RemoveCookie($gebruiker) {
+
+    unset($_COOKIE[$gebruiker]);
+    setcookie($gebruiker, '', time() - 3600);
+    if(isset($_COOKIE[$gebruiker])) {
+        echo "<script>alert('Cookie $gebruiker bestaat nog');</script>";
+    } else {
+        echo "<script>alert('Cookie $gebruiker verwijderd');</script>";
+    }
+}
+
+function showCookies() {
+    print_r($_COOKIE);
+}
+?>
+</body>
 </html>
