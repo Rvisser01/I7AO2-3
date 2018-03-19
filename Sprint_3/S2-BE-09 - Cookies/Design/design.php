f844e8fdd4ddc232cde6512deda0ab177df45465
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-19 14:49:42 +0100

    dsa

--- Sprint_3/S2-BE-09 - Cookies/Design/design.php
+++ Sprint_3/S2-BE-09 - Cookies/Design/design.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="nl" dir="ltr">
   <head>
+    <link rel="stylesheet" type="text/css" href="css/cookies1.css">
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
-    <?php
-      
-    ?>
+    <div class="container-login100" style="background-image: url('../img/Achtergrond.jpeg');">
+
+    </div>
   </body>
 </html>
