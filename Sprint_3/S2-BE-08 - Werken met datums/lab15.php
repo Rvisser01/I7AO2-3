0508af58b165434b6bc98c05f6246976f61ece46
Pimhoomans <36029361+Pimhoomans@users.noreply.github.com>
2018-03-19 09:20:24 +0100

    gresrg

--- Sprint_3/S2-BE-08 - Werken met datums/lab15.php
+++ Sprint_3/S2-BE-08 - Werken met datums/lab15.php
@@ -1,9 +1,9 @@
 <?php
 // date_diff
-$factuurdatum = date_create("2018-10-10");
-echo "<br>Factuurdatum: ".date_format($factuurdatum, "Y-m-d");
+$datum = date_create("1998-01-20");
+echo "<br>datum: ".date_format($datum, "Y-m-d");
 $vandaag = date_create("now");
 echo "<br>Vandaag: ".date_format($vandaag, "Y-m-d");
-$interval = date_diff($vandaag, $factuurdatum);
-echo "<br>U heeft nog: ".$interval->format("%R%a dagen om te betalen");
+$interval = date_diff($vandaag, $datum);
+echo "<br>U bent: ".$interval->format("dagen oud");
 ?>
