b15d4fdccea552de22f43bac8b5af2679d09514d
AHajrizaj <35994818+AHajrizaj@users.noreply.github.com>
2018-03-19 14:34:33 +0100

    gtegrt

---
+++ Sprint_3/S2-BE-09 - JSON en literals/Opdrachten/json.php
@@ -0,0 +1,16 @@
+<?php
+//$user = array (
+//"naam" => "Shyam",
+//"email" => "shyan@nu.nl",
+//"photo" => "shyam.jpg"
+//);
+//echo "<br>userArray : <br>";
+//var_dump($user,true);
+//echo "<br>userJsonliteral: <br>" .$userJsonString;
+
+$boekenJasonLiteral = '[
+  {"Titel" : "Stoner", "auteur" : "John Williams",
+  "genre" : "Fictie", "prijs": 19.99 },
+  
+
+?>
