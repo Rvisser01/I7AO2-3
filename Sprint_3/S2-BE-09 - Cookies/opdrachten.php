78de3ecf7292267d4f117700088778a264d37364
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-19 09:27:25 +0100

    dsa

--- Sprint_3/S2-BE-09 - Cookies/opdrachten.php
+++ Sprint_3/S2-BE-09 - Cookies/opdrachten.php
@@ -6,8 +6,16 @@
   </head>
   <body>
     <?php
-      echo "Maak een cookie";
-      setcookie("gebruiker","sanskrit",mktime(0,0,0,1,1,2050));
+      ob_start();
+
+        echo "Maak een cookie";
+        setcookie("gebruiker","sanskrit",mktime(0,0,0,1,1,2050));
+        $gebruiker = $_COOKIE["gebruiker"];
+        echo "<br>Gebruikersnaam is : $gebruiker";
+        print_r($_COOKIE);
+        ob_end_flush();
+
+
 
     ?>
   </body>
