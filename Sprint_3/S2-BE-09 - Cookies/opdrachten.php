d017aa2946e1bd7071165b4c851687ae58d88bba
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-19 09:18:26 +0100

    ewa

---
+++ Sprint_3/S2-BE-09 - Cookies/opdrachten.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Opdrachten</title>
+  </head>
+  <body>
+    <?php
+      echo "Maak een cookie";
+      setcookie("gebruiker","sanskrit",mktime(0,0,0,1,1,2050));
+
+    ?>
+  </body>
+</html>
