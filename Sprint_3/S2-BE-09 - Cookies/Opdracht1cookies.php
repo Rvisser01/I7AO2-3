d63d461c94f4c95c7f6438abb26f6a4f4cd11105
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-20 13:34:27 +0100

    iohgj

--- Sprint_3/S2-BE-09 - Cookies/Opdracht1cookies.php
+++ Sprint_3/S2-BE-09 - Cookies/Opdracht1cookies.php
@@ -11,22 +11,20 @@
     <div class="inlog">
       <img src="img/Logo.png" id="logo"></img>
       <br>
-        <center>
-          <form method="post">
-            <label>Gebruikersnaam</label>
+      <center>
+        <form method="post">
+          <label id="gn">Gebruikersnaam</label>
           <div class="text-input">
             <input type="text" id="input1" name="gebruiker">
           </div>
-          <label>Wachtwoord</label>
+          <label id="pw">Wachtwoord</label>
           <div class="text-input">
-            <input type="password" id="input1" name="wachtwoord">
+            <input type="password" id="input2" name="wachtwoord">
           </div>
-          </form>
-        </center>
-        <center>
-      <br>
-      <button class="button button1">Login</button>
+        </form>
+        <br>
+        <button class="button button1">Login</button>
+      </center>
     </div>
-    </center>
   </body>
 </html>
