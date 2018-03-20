a6200429ece61a839e9e597eb80ab7e28e0f129d
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-20 11:58:53 +0100

    sda

--- Sprint_3/S2-BE-09 - Cookies/Opdracht1cookies.php
+++ Sprint_3/S2-BE-09 - Cookies/Opdracht1cookies.php
@@ -2,42 +2,28 @@
 <html lang="nl" dir="ltr">
   <head>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/Cookies1.css"
-    <link rel="stylesheet" type="text/css" href="css/extra.css">>
+    <link rel="stylesheet" type="text/css" href="css/Cookies1.css">
     <meta charset="utf-8">
     <title>Cookie Inlog</title>
   </head>
   <body>
-    <center>
-    <div class="inlog">
-      <div class="stage">
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-        <div class="layer"></div>
-      </div>
-      <br>
-        <p class="letter">Gebruikersnaam</p>
-        <form method="post">
-          <input type="text" name="gebruiker" value="">
 
-        </form>
+    <div class="inlog">
+      <img src="img/Logo.png" id="logo"></img>
+      <br>
+        <center>
+          <form method="post">
+            <label>Gebruikersnaam</label>
+          <div class="text-input">
+            <input type="text" id="input1" name="gebruiker">
+          </div>
+          <label>Wachtwoord</label>
+          <div class="text-input">
+            <input type="text" id="input1" name="wachtwoord">
+          </div>
+          </form>
+        </center>
+        <center>
       <br>
       <button class="button button1">Login</button>
     </div>
