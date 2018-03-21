a834db28a7d4fc4ed544d4014ba6c62e9f8c5737
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-21 09:20:26 +0100

    sad

--- Sprint_3/S2-BE-09 - Cookies/opdracht1.php
+++ Sprint_3/S2-BE-09 - Cookies/opdracht1.php
@@ -7,18 +7,18 @@
 <body>
   <h2>Opdracht 1 Cookies</h2>
   <form name="login" method="post" action="login.php">
-   Username: <input type="text" name="username"><br>
-   Password: <input type="password" name="password"><br>
-   Remember Me: <input type="checkbox" name="rememberme" value="1"><br>
+   Username: <input type="text" name="username"><br><?php
+   echo $_COOKIE['remember_me']; ?>">
+   Password: <input type="pass" name="password"><br>
+   Remember Me: <input type="checkbox" name="remember" value="1"><br>
    <input type="submit" name="submit" value="Login!">
   </form>
   <?php
-  /* These are our valid username and passwords */
 
-  $users = array(
-  array("username" => "0279410@student.rocvantwente.nl","0267730@student.rocvantwente.nl"
+
+  $users = array("username" => "0279410@student.rocvantwente.nl","0267730@student.rocvantwente.nl"
   ,"0266931@student.rocvantwente.nl","0300838@student.rocvantwente.nl","0297652@student.rocvantwente.nl",
-  "password" => "Welkom12345!" ));
+  "password" => "Welkom12345!" );
 
 
 
@@ -27,12 +27,12 @@
       if (($_POST['username'] == $users) && ($_POST['password'] == $pass)) {
 
           if (isset($_POST['rememberme'])) {
-              /* Set cookie to last 1 year */
+
               setcookie('username', $_POST['username'], time()+60*60*24*365, '/account', 'www.ao-project.nl');
               setcookie('password', md5($_POST['password']), time()+60*60*24*365, '/account', 'www.ao-project.nl');
 
           } else {
-              /* Cookie expires when browser closes */
+
               setcookie('username', $_POST['username'], false, '/account', 'www.ao-project.nl');
               setcookie('password', md5($_POST['password']), false, '/account', 'www.ao-project.nl');
           }
