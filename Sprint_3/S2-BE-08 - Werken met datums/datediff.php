31e6135fec52d1e11c08fb4bccc2af8f385d5b7d
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-23 12:34:35 +0100

    sdaasd

--- Sprint_3/S2-BE-08 - Werken met datums/datediff.php
+++ Sprint_3/S2-BE-08 - Werken met datums/datediff.php
@@ -6,31 +6,17 @@
   </head>
   <body>
     <h3>Datediff</h3>
-    <form method="post">
-      <input type="text" name="datum1" placeholder="Voer een datum in"><br>
-      Tot<br> <input type="text" name="datum2" placeholder="Voer een datum in">
-      <br><br><input type="submit" name="bereken" value="Bereken">
-    </form>
     <?php
-      if (isset($_POST["bereken"])) {
-        if (!(empty($_POST["datum1"]))) {
-          echo "Fill in a date!";
-        }
-        if (!(empty($_POST["datum2"]))) {
-          echo "Fill in a date!";
-        }
-          $datum1 = $_POST['datum1'];
-          $datum2 = $_POST['datum2'];
+      $datum1 = new DateTime($_POST['datum1']);
+      $datum2 = new DateTime($_POST['datum2']);
 
-          $datum1 = DateTime($datum1);
-          $datum2 = DateTime($datum2);
-          
-          $verschil = date_diff($datum1,$datum2);
-          echo "<br>Er zitten ".$verschil->format("%a dagen verschil tussen");
-      }
-
-
-
-     ?>
+      $diff = date_diff($datum1,$datum2);
+      echo $diff->days;
+    ?>
+    <form method="POST">
+        <input type="date" name="datum1" value="YYYY - MM - DD" />
+        <input type="date" name="datum2" value="YYYY - MM - DD" />
+        <input type="submit" name="submit" value="Verschil"/>
+    </form>
   </body>
 </html>
