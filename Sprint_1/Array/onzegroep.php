0852c1bb60e46a52fefe6596489157d5a2b23d18
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-03-23 10:12:56 +0100

    dsa

--- Sprint_1/Array/onzegroep.php
+++ Sprint_1/Array/onzegroep.php
@@ -9,7 +9,7 @@
       $voornaam = array( '0300013' => "Laurence" , '0266931' => "Peter" , '0267730' => "Pim" , '0279410' => "Arben" , '0297652' => "Robbin" , '0300838' => "Damian");
       $achternaam = array( '0300013' => "Van der Wel" , '0266931' => "Kuipers" , '0267730' => "Hoomans" , '0279410' => "Hajrizaj" , '0297652' => "Visser" , '0300838' => "Trojak");
       $woonplaats = array( '0300013' => "Enschede" , '0266931' => "Enschede" , '0267730' => "Enschede" , '0279410' => "Enschede" , '0297652' => "Overdinkel" , '0300838' => "Enschede");
-      $leeftijd = array( '0300013' => "16" , '0266931' => "20" , '0267730' => "20" , '0279410' => "19" , '0297652' => "16" , '0300838' => "16");
+      $leeftijd = array( '0300013' => "17" , '0266931' => "20" , '0267730' => "20" , '0279410' => "19" , '0297652' => "17" , '0300838' => "16");
 
       $random = array_rand($voornaam, 1);
         echo "Studenummer: $random"."<br>";
