93a9425bb1ca36578389d78606ca111c7360b265
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-04-05 15:12:15 +0200

    ,vhjhvjh

---
+++ Sprint_4/S2-BE-11&12/pdo1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-type"
+    content="text/html;
+    charset=UTF-8">
+    <title></title>
+  </head>
+  <body>
+  <?php
+    echo "<br />---- Opgave 96: Drivers: ";
+    print_r(PDO::getAvailableDrivers());
+
+        echo "<br />---- Opgave 97: PDO verbinding maken.";
+        $dbhost = "localhost";
+        $dbname = "webshop";
+        $user = "root";
+        $pass = "";
+        try {
+          $database = new
+          PDO("mysql:host=$dbhost;dbname=$dbname",$user,$pass);
+          $database->setAttribute
+          (PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
+          echo "<br />Verbinding Gemaakt";
+        }
+        catch (PDOException $e) {
+          echo $e->getMessage();
+          echo "<br />Verbinding NIET Gemaakt";
+        }
+
+  ?>
+  </body>
+</html>
