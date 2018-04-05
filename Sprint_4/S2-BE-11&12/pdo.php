406a11f9fbf487a388ed751ad02354ee5f3b1021
dtrojak <dtrojak01@student.rocvantwente.nl>
2018-04-05 12:02:30 +0200

    dsffd

---
+++ Sprint_4/S2-BE-11&12/pdo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-type"
+    content="text/html;
+    charset=UTF-8">
+    <title></title>
+  </head>
+  <body>
+  <?php
+    echo "<br />---- Opgave 96: Drivers: ";
+    print_r(PDO::getAvailableDrivers());
+  ?>
+  </body>
+</html>
