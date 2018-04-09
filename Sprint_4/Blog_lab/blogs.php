2625ed0f5f0ffce7a78f1b1f597dfd1ba42c1a28
Pimhoomans <36029361+Pimhoomans@users.noreply.github.com>
2018-04-09 09:24:00 +0200

    fdxgh

--- Sprint_4/Blog_lab/blogs.php
+++ Sprint_4/Blog_lab/blogs.php
@@ -9,28 +9,13 @@
  $blogpost = $_POST['blogpost'];
  $blog = $title . "*" . $blogpost;
 
-<<<<<<< HEAD
-//  fwrite($blog,"blogs.txt");
+
+ fwrite($blog,"blogs.txt");
  $bestand = fopen ("blog.txt" , "a");
  if (!$out) {
    header ('location:  ');
    exit;
 
 }
-
-
-$title = $_POST['titel'];
-$blogpost = $_POST['blogpost'];
-$blog = $title . "*" . $blogpost;
-
-fputs ($out,"$title\t");
-fputs ($out,"$comment\n");
-
-fclose ($out);
-header ('Location: blog.php');
-exit;
-=======
-fwrite($blog,"blogs.txt");
-
->>>>>>> fd0f004db9700a79b63d42e19077c8d89a4d8d66
+zdfgxgfgzdf
 ?>
