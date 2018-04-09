f06096edec7b10a30a2f75384a537d5ad395ba15
AHajrizaj <35994818+AHajrizaj@users.noreply.github.com>
2018-04-09 09:12:32 +0200

    ....

--- Sprint_4/Blog_lab/blogs.php
+++ Sprint_4/Blog_lab/blogs.php
@@ -1,31 +1,30 @@
-//<?php
-//  $bestand = fopen("blogs.txt", "ab");
+<?php
+ $bestand = fopen("blogs.txt", "ab");
+ if(!$bestand) {
+    echo "Kon geen bestand openen";
+  }
 
-//  if(!$bestand) {
-//    echo "Kon geen bestand openen";
-//  }
+ $title = $_POST['titel'];
+  $blogpost = $_POST['blogpost'];
+  $blog = $title . "*" . $blogpost;
 
-//  $title = $_POST['titel'];
-//  $blogpost = $_POST['blogpost'];
-//  $blog = $title . "*" . $blogpost;
-
-//  fwrite($blog,"blogs.txt");
- $bestand = fopen ("blog.txt" , "a");
- if (!$out) (
+  fwrite($blog,"blogs.txt");
+  $bestand = fopen ("blog.txt" , "w");
+  if (!$out) (
    header ('location:  ')
     exit;
 
 )
 
 
-$title = $_POST['titel'];
-$blogpost = $_POST['blogpost'];
-$blog = $title . "*" . $blogpost;
+//$title = $_POST['titel'];
+//$blogpost = $_POST['blogpost'];
+//$blog = $title . "*" . $blogpost;
 
-fputs ($out,"$title\t");
-fputs ($out,"$comment\n");
+//fputs ($out,"$title\t");
+//fputs ($out,"$comment\n");
 
-fclose ($out);
-header ('Location: blog.php');
-exit;
+//fclose ($out);
+//header ('Location: blog.php');
+//exit;
 ?>
