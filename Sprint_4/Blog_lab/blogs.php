637fa8577733d9979aedf0bc2c9a7ce618d52a29
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-04-09 09:00:16 +0200

    sad

--- Sprint_4/Blog_lab/blogs.php
+++ Sprint_4/Blog_lab/blogs.php
@@ -1,31 +1,14 @@
-//<?php
-//  $bestand = fopen("blogs.txt", "ab");
+<?php
+  $bestand = fopen("blogs.txt", "ab");
 
-//  if(!$bestand) {
-//    echo "Kon geen bestand openen";
-//  }
+ if(!$bestand) {
+    echo "Kon geen bestand openen";
+  }
 
-//  $title = $_POST['titel'];
-//  $blogpost = $_POST['blogpost'];
-//  $blog = $title . "*" . $blogpost;
+  $title = $_POST['titel'];
+ $blogpost = $_POST['blogpost'];
+ $blog = $title . "*" . $blogpost;
 
-//  fwrite($blog,"blogs.txt");
- $bestand = fopen ("blog.txt" , "a");
- if (!$out) (
-   header ('location:  ')
-    exit;
+fwrite($blog,"blogs.txt");
 
-)
-
-
-$title = $_POST['titel'];
-$blogpost = $_POST['blogpost'];
-$blog = $title . "*" . $blogpost;
-
-fputs ($out,"$title\t");
-fputs ($out,"$comment\n");
-
-fclose ($out);
-header ('Location: blog.php');
-exit;
 ?>
