273320974a50240b064e807728b90937fe175b0c
LaurencevdWel <lvanderwel01@student.rocvantwente.nl>
2018-04-13 08:29:22 +0200

    ewq

--- Sprint_4/Schiphol/formulier.php
+++ Sprint_4/Schiphol/formulier.php
@@ -1,83 +1,139 @@
+<?php
+//Connect to DB
+$con = mysqli_connect("vserver275.axc.nl", "robbiwn275_schiphol", "robbin", "robbiwn275_schiphol") or die("couldn't connect to the database!");
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-  <title>Schiphol</title>
-
+<title>Schiphol</title>
 <!-- Font Awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-
 <!-- Bootstrap core CSS -->
 <link href="https://mdbootstrap.com/previews/docs/latest/css/bootstrap.min.css" rel="stylesheet">
-
 <!-- Material Design Bootstrap -->
 <link href="https://mdbootstrap.com/previews/docs/latest/css/mdb.min.css" rel="stylesheet">
-
-
-    <link rel="stylesheet" href="css/main.css">
-
+<link rel="stylesheet" href="css/main.css">
 <meta charset="utf-8">
 </head>
 <body>
   <div class="view overlay zoom">
       <img src="img/header-schiphol.jpg" class="img-fluid" alt="zoom" width="100%">
-      <div class="mask flex-center waves-effect waves-light">
-      </div>
+      <div class="mask flex-center waves-effect waves-light"></div>
   </div>
-
 <div class="container">
 <div class="view overlay zoom">
-
 </div>
+<!-- Default klacht -->
+              <form method="post">
+                <br>
+                <center>
+                  <p class="h4 mb-4">Klachten Formulier</p>
+                  <?php
 
+                  if (isset($_POST['terug'])) {
+                    echo "<script>setTimeout(function () {location.href = 'index.html'; }, 0);</script>";
+                  }
 
+                  if (isset($_POST['send'])) {
+                    $naam = $_POST['naam'];
+                    $tussen = $_POST['tussen'];
+                    $achternaam = $_POST['achternaam'];
+                    $adres = $_POST['adres'];
+                    $postcode = $_POST['postcode'];
+                    $email = $_POST['email'];
+                    $klachtText = $_POST['klacht'];
 
+                    $error = false;
 
-              <!-- Default klacht -->
-              <form>
-                  <p class="h4 text-center mb-4">Klachten Formulier</p>
+                    if (!($naam == "")) {
 
+                    } else {
+                      //naam leeg
+                      $error = true;
+                    }
+
+                    if (!($achternaam == "")) {
+
+                    } else {
+                      //achternaam leeg
+                      $error = true;
+                    }
+
+                    if (!($adres == "")) {
+
+                    } else {
+                      //adres leeg
+                      $error = true;
+                    }
+
+                    if (!($postcode == "")) {
+
+                    } else {
+                      //postcode leeg
+                      $error = true;
+                    }
+
+                    if (!($email == "")) {
+
+                    } else {
+                      //email leeg
+                      $error = true;
+                    }
+
+                    if (!($klachtText == "")) {
+
+                    } else {
+                      //klacht leeg
+                      $error = true;
+                    }
+
+                    if ($error == false) {
+                      $sql = "INSERT INTO `klachten`(`naam`, `tussenvoegsel`, `achternaam`, `adres`, `postcode`, `email`,`klacht`) VALUES ('$naam','$tussen','$achternaam','$adres', '$postcode', '$email', '$klachtText')";
+                      $run = mysqli_query($con, $sql);
+
+                      echo "<script>setTimeout(function () {location.href = 'klachten.php'; }, 0);</script>";
+                    } else {
+                      echo "<br>";
+                      echo "<p>Vul alle vakken in met een *!</p>";
+                    }
+                  }
+
+                   ?>
+                </center>
                   <!-- Default input name -->
                   <label for="defaultFormContactNameEx" class="grey-text">Naam</label>
-                  <input type="text" id="defaultFormContactNameEx" class="form-control" name="Naam">
-
+                  <input type="text" id="defaultFormContactNameEx" class="form-control" name="naam">
                   <br>
-
+                  <!-- Default input email -->
+                  <label for="defaultFormContactEmailEx" class="grey-text">Tussenvoegsel</label>
+                    <input type="Text" id="defaultFormContactEmailEx" class="form-control" name="tussen">
+                  <br>
                   <!-- Default input email -->
                   <label for="defaultFormContactEmailEx" class="grey-text">Achternaam</label>
-                  <input type="Text" id="defaultFormContactEmailEx" class="form-control" name="Achternaam">
-
+                    <input type="Text" id="defaultFormContactEmailEx" class="form-control" name="achternaam">
                   <br>
-
                   <!-- Default input subject -->
                   <label for="defaultFormContactSubjectEx" class="grey-text">Adres</label>
-                  <input type="text" id="defaultFormContactSubjectEx" class="form-control" name="Adres">
-
+                    <input type="text" id="defaultFormContactSubjectEx" class="form-control" name="adres">
                   <br>
-
-				  <!-- Default input name -->
+				          <!-- Default input name -->
                   <label for="defaultFormContactNameEx" class="grey-text">Postcode</label>
-                  <input type="text" id="defaultFormContactNEx" class="form-control" name="Postcode">
-
-				  <br>
-
-				  <!-- Default input name -->
-                  <label for="defaultFormContactNameEx" class="grey-text">Telefoonnummer</label>
-                  <input type="text" id="defaultFormContactNaeEx" class="form-control" name="Telefoonnummer">
-
-				  <br>
-
+                    <input type="text" id="defaultFormContactNEx" class="form-control" name="postcode">
+				          <br>
+                  <!-- Default input name -->
+                  <label for="defaultFormContactNameEx" class="grey-text">Email</label>
+                    <input type="text" id="defaultFormContactNaeEx" class="form-control" name="email">
+				          <br>
                   <!-- Default textarea message -->
                   <label for="defaultFormContactMessageEx" class="grey-text">Klacht</label>
-                  <textarea type="text" id="defaultFormContactMessageEx" class="form-control" rows="3" name="Klachtformulier"></textarea>
+                    <textarea type="text" id="defaultFormContactMessageEx" class="form-control" rows="3" name="klacht"></textarea>
 
+                  <!-- Buttons -->
                   <div class="text-center mt-4">
-				   <button class="btn btn-outline-warning" type="Back">Terug<i></i></button>
-                      <button class="btn btn-outline-warning" type="submit">Verzend<i class="fa fa-paper-plane-o ml-2"></i></button>
-
-				  </div>
+                      <input type="submit" class="btn btn-outline-default" name="terug" value="Terug">
+                      <input type="submit" class="btn btn-outline-default" name="send" value="Verzend">
+				          </div>
               </form>
               <!-- Default klacht -->
               <br>
